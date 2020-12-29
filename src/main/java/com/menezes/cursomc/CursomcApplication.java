2f582f7f546c0a6184ab3d8997cacb83079bf8ea
Jessica Menezes <jessica_menezes05@hotmail.com>
2020-12-28 21:43:02 -0300

    Criando operacao de instanciacao

--- src/main/java/com/menezes/cursomc/CursomcApplication.java
+++ src/main/java/com/menezes/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package com.menezes.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.menezes.cursomc.domain.Categoria;
+import com.menezes.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria("Informática", null);
+		Categoria cat2 = new Categoria("Escritório", null);
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
