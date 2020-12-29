f7ea7541572b582aac7bea8299f18335d7327681
Jessica Menezes <jessica_menezes05@hotmail.com>
2020-12-28 21:31:53 -0300

    Criando repository e service para Categoria

--- src/main/java/com/menezes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/menezes/cursomc/resources/CategoriaResource.java
@@ -3,29 +3,28 @@ package com.menezes.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.menezes.cursomc.domain.Categoria;
+import com.menezes.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria("Informatica", 1);
-		Categoria cat2 = new Categoria("Escritorio", 2);
-		
-		List<Categoria> lista = new ArrayList<>(); 
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
 	
-	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 
 }
---
+++ src/main/java/com/menezes/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.menezes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.menezes.cursomc.domain.Categoria;
+import com.menezes.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {     
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null); 
+	}
+
+
+}
